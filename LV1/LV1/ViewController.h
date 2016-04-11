6e635d79372eac893babb6e053bed797384c26dd
Dino <dkurtagic@gmail.com>
2016-04-11 20:16:14 +0200

    Inicijalni komit

---
+++ LV1/LV1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LV1
+//
+//  Created by Dino Kurtagic on 11/04/16.
+//  Copyright © 2016 Lycheesys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LV1/LV1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LV1
+//
+//  Created by Dino Kurtagic on 11/04/16.
+//  Copyright © 2016 Lycheesys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
